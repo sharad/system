00809ab60e8adbcf0440fc7b70724a993905f89b
Sharad Pratap <sh4r4d@gmail.com>
2016-01-05 03:04:12 +0530

    corrections

--- ubuntu/usr/local/var/lib/jvpn/sbin/disconnect_hook.sh
+++ ubuntu/usr/local/var/lib/jvpn/sbin/disconnect_hook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo cp /etc/bind/named.conf.forwarders-jvpn /etc/bind/named.conf.forwarders
-sudo service bind9 reload
-
+if [ -r /etc/bind/named.conf.forwarders-jvpn ]
+then
+    sudo cp /etc/bind/named.conf.forwarders-jvpn /etc/bind/named.conf.forwarders
+    sudo service bind9 reload
+    sudo rm -f /etc/bind/named.conf.forwarders-jvpn
+else
+    echo /etc/bind/named.conf.forwarders-jvpn do not exists, not doing anything. >&2
+fi
